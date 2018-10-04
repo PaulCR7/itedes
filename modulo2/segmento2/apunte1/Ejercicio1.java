d404fc1c7be39bdeaf267306318ede0fa6c9dc78
alumno <alumno@debian.BFACONSULTORA>
2018-10-04 20:25:26 -0300

    commit inicial

---
+++ modulo2/segmento2/apunte1/Ejercicio1.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+public class Ejercicio1.{
+	public static void main(String args[]){
+	scanner teclado= new Scanner(System.in)
+	system.out.print("ingrese barrio: ");
+	String barrio= teclado.nextLine();
+
+	system.out.print("ingrese direccion: ");
+	String direccion1= teclado.nextLine();
+
+	system.out.print("ingrese entrecalle: ");
+	String entrecalle1= teclado.nextLine();
+
+	system.out.print("ingrese entrecalle: ");
+	String entrecalle2= teclado.nextLine();
+
+	System.out.printLn("La comisaria de " +barrio+ "queda en la calle " +direccion1+ "entre " +entrecalle1+ "y " +entrecalle2+ ".");
+	}
+}
+
---
+++ modulo2/segmento2/apunte1/ejercicio1.net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ejercicio1.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Como te llamas: ");
+			String nombre= Console.ReadLine();
+			
+			Console.WriteLine("hola "+nombre);
+        }
+    }
+}
